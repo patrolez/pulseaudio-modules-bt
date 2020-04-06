aed1b7955f1fc42240ef0f3452e295e1d48152d8
patrolez <patrol9@gmail.com>
2020-04-06 03:07:14 +0200

    Allow non-sudo users to run the script if they have permissions to Docker

--- build-scripts/build.bash
+++ build-scripts/build.bash
@@ -2,8 +2,9 @@
 : ${1?"$(printf '%s\n' "Need a parameter! Which distribution would you like to build?" "$( for fn in *.Dockerfile; do printf '> %s\n' "${fn%.Dockerfile}"; done )")"}
 
 ensure_that_has_permissions() {
-    if ! sudo docker version
+    if ! docker version
     then
+        echo >&2
         echo "Most likely you have no permisison to use docker! Aborting!" >&2
         exit 1
     fi
@@ -13,15 +14,14 @@ main() {
     local TGT="$1"
     (
         set -e
-        sudo docker build -t "tmp.remove.${TGT}" -f "${TGT}.Dockerfile" .
-        sudo docker run --rm --entrypoint /bin/bash "tmp.remove.${TGT}" -c 'tar -cf - /dist' | tar -x --no-same-owner
+        docker build -t "tmp.remove.${TGT}" -f "${TGT}.Dockerfile" .
+        local RESPECTIVE_USR=$(id -u "$(logname)")
+        local RESPECTIVE_USR_GRP=$(id -g "${RESPECTIVE_USR}")
+        docker run --rm --entrypoint /bin/bash "tmp.remove.${TGT}" -c \
+            "tar -cf - /dist --owner=${RESPECTIVE_USR} --group=${RESPECTIVE_USR_GRP}" | tar -x
         echo "This script will not clean-up itself Docker Images!" >&2
     )
 }
 
-if [[ $EUID -eq 0 ]]; then
-   echo "Script sould be run as a user! Not root!" >&2
-   exit 1
-fi
 ensure_that_has_permissions
 main "$@"
